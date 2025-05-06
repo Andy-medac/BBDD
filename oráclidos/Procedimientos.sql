b5986c9c3518aeec263ada772ac39c19e1744faf
andy <aoa0008@alu.medac.es>
2025-05-06 10:50:55 +0200

    no quiero más

--- oráclidos/Procedimientos.sql
+++ oráclidos/Procedimientos.sql
@@ -1,7 +1,6 @@
 // 1. Realizar un procedimiento que muestre cuantos modelos de coches hay de la marca 'Citroen'.
 
 CREATE OR REPLACE PROCEDURE mostrar_modelos AS
-
     cantidad   NUMBER;
     v_id_marca modelo_coche.id_marca%TYPE;
     v_marca    marcas_coche.marca%TYPE := &marca;
@@ -20,10 +19,10 @@ END;
 
 EXEC mostrar_modelos;
 
-CREATE OR REPLACE PROCEDURE mostrar_modelos (v_marca   marcas_coche.marca%TYPE)
-AS
-    cantidad   NUMBER;
-    
+CREATE OR REPLACE PROCEDURE mostrar_modelos (
+    v_marca marcas_coche.marca%TYPE
+) AS
+    cantidad NUMBER;
 BEGIN
     SELECT
         COUNT(mo.id_marca)
@@ -39,7 +38,51 @@ END;
 
 /* EJECUTARLO */
 DECLARE
-    v_marca   marcas_coche.marca%TYPE := &marca;
+    v_marca marcas_coche.marca%TYPE := &marca;
 BEGIN
     mostrar_modelos(v_marca);
 END;
+
+/*4. Crear un procedimiento ActualizarPrecioCoche que acepte un nuevo_precio y matrícula como parámetro.
+Antes de realizar la actualización, se verifica si la matrícula existe en la tabla coche. 
+Si el la matrícula no existe, guardar una excepción controlada personalizada con RAISE y mostrar un mensaje de error usando DBMS_OUTPUT. 
+(Podéis hacer un select que cuenta los coches con la matrícula pasada por parámetro, en caso de que sea 0 ya sabes que no existe por lo que se puede generar la excepción)
+Utilizar una estructura de control de flujo para verificar si el nuevo_precio es válido, por ejemplo mayor que 0 y menos que 10000.
+Si el precio es válido, actualizar la columna precio_compra de la tabla coche cuya matrícula sea la que hemos pasado por parámetro.
+Si el precio no es válido, guardar una excepción controlada personalizada con RAISE y mostrar un mensaje de error usando DBMS_OUTPUT.*/
+
+DECLARE
+    v_precio    coche.precio_compra%TYPE := &precio;
+    v_matricula coche.matricula%TYPE := &matricula;
+BEGIN
+    actualizarpreciocoche(v_precio, v_matricula);
+END;
+
+CREATE OR REPLACE PROCEDURE ActualizarPrecioCoche (nuevo_precio coche.precio_compra%TYPE, 
+                                                                                            v_matricula coche.matricula%TYPE) AS
+v_count NUMBER;
+errorCoche EXCEPTION;
+errorPrecio EXCEPTION;
+BEGIN
+SELECT COUNT(*) INTO v_count FROM coche WHERE
+    matricula = v_matricula;
+    IF v_count = 0 THEN
+        RAISE errorcoche;
+    END IF;
+    IF nuevo_precio > 0 AND nuevo_precio < 10000 THEN 
+    UPDATE coche SET precio_compra = nuevo_precio WHERE matricula = v_matricula;
+            dbms_output.put_line('Precio actualizado');
+            ELSE 
+            RAISE errorPrecio;
+    END IF; EXCEPTION
+    WHEN errorcoche THEN
+        dbms_output.put_line('No se encontró esa matrícula || v_matricula');
+    WHEN errorprecio THEN
+        dbms_output.put_line('El nuevo precio '
+                             || nuevo_precio
+                             || ' no es correcto');
+    WHEN OTHERS THEN 
+            dbms_output.put_line('Error: ' || SQLERRM);
+END;
+
+select * from coche;
--- oráclidos/disparadores.sql
+++ oráclidos/disparadores.sql
@@ -30,3 +30,20 @@ CREATE OR REPLACE TRIGGER fechaactual BEFORE
 BEGIN
     :new.fecha_insercion := sysdate;
 END;
+/
+/*4. Crear un trigger que, antes de actualizar un coche, revise si el campo 
+precio_compra ha cambiado, y si es así, le aplique un 21% de IVA 
+automáticamente (es decir, precio_compra := precio_compra * 1.21), 
+siempre que el nuevo valor no sea NULL.*/
+
+CREATE OR REPLACE TRIGGER iva_coche BEFORE
+    UPDATE OF precio_compra ON coche /*tambien se puede poner UPDATE ON coche*/
+    FOR EACH ROW
+BEGIN
+    IF :old.precio_compra != :new.precio_compra AND :new.precio_compra IS NOT NULL THEN
+        :new.precio_compra := :new.precio_compra*1.21;
+    END IF;
+END;
+/
+UPDATE coche SET precio_compra = 33445 WHERE matricula = '3345GVF';
+SELECT * FROM coche;
